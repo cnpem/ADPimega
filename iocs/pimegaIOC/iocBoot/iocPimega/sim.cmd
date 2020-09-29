e97b7fb6eed0f623403abb443e97a9b35f3b4b33
Dawood Awny Dawood Alnajjar <dawood.alnajjar@bertha.abtlus.org.br>
2020-09-29 18:31:09 -0300

    changed h5 to hdf5. disabled procServer temporarily from both sim and normal.

--- iocs/pimegaIOC/iocBoot/iocPimega/sim.cmd
+++ iocs/pimegaIOC/iocBoot/iocPimega/sim.cmd
@@ -78,7 +78,7 @@ iocInit()
 
 dbpf(${PREFIX}cam1:FilePath,"/tmp")
 dbpf(${PREFIX}cam1:FileName,"teste")
-dbpf(${PREFIX}cam1:FileTemplate,"%s%s_%3.3d.h5")
+dbpf(${PREFIX}cam1:FileTemplate,"%s%s_%3.3d.hdf5")
 
 # save things every thirty seconds
 #create_monitor_set("auto_settings.req", 30,"P=$(PREFIX)")
