b13f65763deccfe5851ce244d6cd462b6477f1eb
Dawood Awny Dawood Alnajjar <dawood.alnajjar@bertha.abtlus.org.br>
2021-05-14 07:51:00 -0300

    Disabled automatic stats2 enable.

--- iocs/pimegaIOC/iocBoot/iocPimega/st-cnb-d-pimega135d-2.cmd
+++ iocs/pimegaIOC/iocBoot/iocPimega/st-cnb-d-pimega135d-2.cmd
@@ -88,7 +88,7 @@ dbpf(${PREFIX}cam1:FileTemplate,"%s%s_%3.3d.hdf5")
 dbpf(${PREFIX}cam1:dac_defaults_files,"/usr/local/epics/synApps/support/areaDetector-R3-3-1/ADPimega/iocs/pimegaIOC/iocBoot/iocPimega/config/pimega135d-2.ini")
 dbpf(${PREFIX}cam1:ImgChipNumberID, 1)
 dbpf(${PREFIX}image1:EnableCallbacks, 1)
-dbpf(${PREFIX}Stats2:EnableCallbacks, 1)
+#dbpf(${PREFIX}Stats2:EnableCallbacks, 1)
 dbpf(${PREFIX}cam1:LoadEqualization, 60)
 # save things every thirty seconds
 #create_monitor_set("auto_settings.req", 30,"P=$(PREFIX)")
