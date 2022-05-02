f9abebb7d3015769eb889603387e8078167b3252
robert <robert.tartarotti@pitec.co>
2022-05-02 16:04:34 -0300

    improvements on epics base install.sh

--- epicsbase/install.sh
+++ epicsbase/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-
-CORES=$(lscpu | grep "CPU(s):      " | awk '{ print $2; }')
+cp epics.sh /etc/profile.d/
+chmod +x /etc/profile.d/epics.sh
+echo ". /etc/profile.d/epics.sh" >> /etc/bash.bashrc
+arch | xargs -i@ echo "/usr/local/epics/base/lib/linux-@" > /etc/ld.so.conf.d/epics.conf
 cd /tmp
 wget https://epics.anl.gov/download/base/base-3.15.6.tar.gz
 tar -xzf base-3.15.6.tar.gz
 mkdir /usr/local/epics
-mv base-3.15.6 /usr/local/epics
-cd /usr/local/epics/base-3.15.6
-make -j $CORES
-ln -s /usr/local/epics/base-3.15.6 /usr/local/epics/base
+mv base-3.15.6 /usr/local/epics/base
+cd /usr/local/epics/base
+make -j
+ln -s /usr/local/epics/base /usr/local/epics/base
 rm /tmp/base-3.15.6.tar.gz
