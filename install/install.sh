bd3a309be76e78c6eb7d9488b86cbac03823e2f6
robert <robert.tartarotti@pitec.co>
2022-05-02 18:36:09 -0300

    [Added instal.sh for synapps and base]

--- install/install.sh
+++ install/install.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 cp epics.sh /etc/profile.d/
+cp RELEASE /tmp/
+cp RELEASE_motor-r7-1 /tmp/
 chmod +x /etc/profile.d/epics.sh
 echo ". /etc/profile.d/epics.sh" >> /etc/bash.bashrc
 arch | xargs -i@ echo "/usr/local/epics/base/lib/linux-@" > /etc/ld.so.conf.d/epics.conf
 cd /tmp
 wget https://epics.anl.gov/download/base/base-3.15.6.tar.gz
 tar -xzf base-3.15.6.tar.gz
+wget https://epics.anl.gov/bcda/synApps/tar/synApps_6_1.tar.gz
+tar -xvf synApps_6_1.tar.gz
 mkdir /usr/local/epics
 mv base-3.15.6 /usr/local/epics/base
+mv synApps_6_1 /usr/local/epics/synApps
+cp RELEASE /usr/local/epics/synApps/support/configure/RELEASE
+cp RELEASE_motor-r7-1 /usr/local/epics/synApps/support/motor-R7-1/configure/RELEASE
 cd /usr/local/epics/base
 make -j
 ln -s /usr/local/epics/base /usr/local/epics/base
-rm /tmp/base-3.15.6.tar.gz
+cd /usr/local/epics/synApps/support
+make release
+make
+
+
