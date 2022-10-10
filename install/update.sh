5278ad3ae8f7c21393fb123504afcd609fd6debd
Sara <sara.santos@pitec.co>
2022-10-10 15:23:36 -0300

    [PSS-1185] Fix for remaining time on IOC

---
+++ install/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo rm -rf /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
+sudo cp -r ../../adpimega/ /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
+cd $PIMEGA_PSS/api
+bash build.sh -g
+sudo bash build.sh -e
+cd /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
+sudo make
+cd iocs
+sudo make
+cd pimegaIOC
+sudo make
+cd iocBoot
+sudo make
