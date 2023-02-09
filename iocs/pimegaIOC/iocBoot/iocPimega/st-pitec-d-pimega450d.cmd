aefa19b0e39c6ac79bb11c6b6d0ea6fdc37243d1
Sara Mirthis <sara.santos@pitec.co>
2023-02-09 15:07:02 -0300

    Hotfix for 450d epics ioc

--- iocs/pimegaIOC/iocBoot/iocPimega/st-pitec-d-pimega450d.cmd
+++ iocs/pimegaIOC/iocBoot/iocPimega/st-pitec-d-pimega450d.cmd
@@ -11,7 +11,7 @@ epicsEnvSet("PREFIX", "PITEC:D:PIMEGA450D:")
 # The port name for the detector
 epicsEnvSet("PORT",   "PIMEGA")
 # The detector model (0:mobipix; 1:pimega45D; 2:pimega135DL; 3:pimega135D; 4:pimega540D, 5:pimega450D)
-epicsEnvSet("DMODEL", "4");
+epicsEnvSet("DMODEL", "5");
 # The queue size for all plugins
 epicsEnvSet("QSIZE",  "20")
 # The maximim image width; used for row profiles in the NDPluginStats plugin
@@ -40,11 +40,11 @@ epicsEnvSet("PIMEGA_MODULE02_IP", "223.255.255.3")
 epicsEnvSet("PIMEGA_MODULE03_IP", "223.255.255.4")
 epicsEnvSet("PIMEGA_MODULE04_IP", "223.255.255.5")
 epicsEnvSet("PIMEGA_MODULE05_IP", "223.255.255.6")
-epicsEnvSet("PIMEGA_MODULE06_IP", "223.255.255.7")
-epicsEnvSet("PIMEGA_MODULE07_IP", "223.255.255.8")
-epicsEnvSet("PIMEGA_MODULE08_IP", "223.255.255.9")
-epicsEnvSet("PIMEGA_MODULE09_IP", "223.255.255.10")
-epicsEnvSet("PIMEGA_MODULE10_IP", "223.255.255.11")
+epicsEnvSet("PIMEGA_MODULE06_IP", "223.255.254.2")
+epicsEnvSet("PIMEGA_MODULE07_IP", "223.255.254.3")
+epicsEnvSet("PIMEGA_MODULE08_IP", "223.255.254.4")
+epicsEnvSet("PIMEGA_MODULE09_IP", "223.255.254.5")
+epicsEnvSet("PIMEGA_MODULE10_IP", "223.255.254.6")
 # The IP port for the command socket
 epicsEnvSet("PIMEGA_PORT", "60000")
 # The search path for database files
