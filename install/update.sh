50caf4cad90b06cb56f7f7391d97ec3dde5195e5
robert <robert.tartarotti@pitec.co>
2023-02-15 09:35:58 -0300

    [] Remove API generation (cmakes) from update.sh file of the epics

--- install/update.sh
+++ install/update.sh
@@ -3,7 +3,6 @@
 sudo rm -rf /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
 sudo cp -r ../../epics /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
 cd $PIMEGA_PSS/api
-bash build.sh -g
 sudo bash build.sh -e
 cd /usr/local/epics/synApps/support/areaDetector-R3-7/ADPimega
 sudo make
